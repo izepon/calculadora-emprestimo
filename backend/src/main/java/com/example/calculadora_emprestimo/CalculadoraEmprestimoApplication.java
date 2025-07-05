ae086ab282c49afaf293305a9596b200f07b5363
Jean Izepon <izeponjc@gmail.com>
2025-07-05 00:48:53 -0300

    adicionado estrutura inicial do backend

---
+++ backend/src/main/java/com/example/calculadora_emprestimo/CalculadoraEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadora_emprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraEmprestimoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/calculadora_emprestimo/CalculadoraEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadora_emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
