9dabcda7d587786da8f11125af2db6fa406195f6
Jean Izepon <izeponjc@gmail.com>
2025-07-05 15:24:32 -0300

    ajustado retorno do json

--- backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoResponse.java
+++ backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoResponse.java
@@ -5,5 +5,13 @@ import java.time.LocalDate;
 
 public record SimulacaoResponse(
         LocalDate data,
-        BigDecimal valor
+        BigDecimal valorEmprestimo,
+        BigDecimal saldoDevedor,
+        String consolidada,
+        BigDecimal total,
+        BigDecimal amortizacao,
+        BigDecimal saldo,
+        BigDecimal provisao,
+        BigDecimal acumulado,
+        BigDecimal pago
 ) {}
