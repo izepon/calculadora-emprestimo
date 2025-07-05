f50fa2816398a3faf4fd81b208b7df209e7e4841
Jean Izepon <izeponjc@gmail.com>
2025-07-05 10:52:57 -0300

    adicionado uso do record

--- backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoRequest.java
+++ backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoRequest.java
@@ -1,24 +1,15 @@
 package com.izepon.calculadora_emprestimo.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter
-@Setter
-public class SimulacaoRequest {
-    private LocalDate dataInicial;
-    private LocalDate dataFinal;
-    private LocalDate primeiroPagamento;
-    private BigDecimal valorEmprestimo;
-    private BigDecimal taxaJuros;
-
-    public SimulacaoRequest(LocalDate dataInicial, LocalDate dataFinal, LocalDate primeiroPagamento, BigDecimal valorEmprestimo, BigDecimal taxaJuros) {
-        this.dataInicial = dataInicial;
-        this.dataFinal = dataFinal;
-        this.primeiroPagamento = primeiroPagamento;
-        this.valorEmprestimo = valorEmprestimo;
-        this.taxaJuros = taxaJuros;
-    }
-}
+public record SimulacaoRequest(
+        @NotNull LocalDate dataInicial,
+        @NotNull LocalDate dataFinal,
+        @NotNull LocalDate primeiroPagamento,
+        @NotNull @DecimalMin(value = "0.0", inclusive = false) BigDecimal valorEmprestimo,
+        @NotNull @DecimalMin(value = "0.0", inclusive = false) BigDecimal taxaJuros
+) {}
--- backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoResponse.java
+++ backend/src/main/java/com/izepon/calculadora_emprestimo/dto/SimulacaoResponse.java
@@ -1,19 +1,9 @@
 package com.izepon.calculadora_emprestimo.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter
-@Setter
-public class SimulacaoResponse {
-
-    private LocalDate data;
-    private BigDecimal valor;
-
-    public SimulacaoResponse(LocalDate data, BigDecimal valor) {
-        this.data = data;
-        this.valor = valor;
-    }
-}
+public record SimulacaoResponse(
+        LocalDate data,
+        BigDecimal valor
+) {}
