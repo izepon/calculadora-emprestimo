0fa63044c9f259b9415a49426a4d7dd96a3c537c
Jean Izepon <izeponjc@gmail.com>
2025-07-05 21:14:09 -0300

    resolvido problema de case sensitive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import index from "./pages/index";
+import notFound from "./pages/notFound";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +15,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<index />} />
           { }
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<notFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
